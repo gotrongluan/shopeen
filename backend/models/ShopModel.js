6ddd182866c3db6cb291904b821bca139cea99df
quan5609 <quan.buicompscibk@hcmut.edu.vn>
2020-08-03 00:31:32 +0700

    * update shop schema

--- backend/models/ShopModel.js
+++ backend/models/ShopModel.js
@@ -4,6 +4,17 @@ const Schema = mongoose.Schema;
 var ShopSchema = mongoose.Schema({
   name: { type: String, required: true, unique: true },
   address: { type: String, required: true, unique: true },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
   category: { type: String, enum: ['food', 'beverage', 'clothes'] },
   description: { type: String },
   owner: { type: Schema.Types.ObjectId, ref: 'User' },
